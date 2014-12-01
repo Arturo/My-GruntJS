5494dd22b197cb8d9bf2bfe0fdb745b970ec6c06
Artur <janicki.artur@gmail.com>
2014-12-01 22:26:39 +0100

    change conf for test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,3 +4,14 @@ angular.module('App', [
         'ui.router',
         'ngAnimate'
     ])
+
+.config(['$stateProvider', '$urlRouterProvider', '$provide', function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider
+        .state('login', {
+          url: '/login',
+          templateUrl: '',
+          controller: 'LoginCtrl'
+        })
+}])
